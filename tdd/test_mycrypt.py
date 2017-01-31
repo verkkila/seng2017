2d27739d7e4fa7c12b2f1032c195010e0a40b011
Pekka Pietikäinen <pekka@pietikainen.org>
2017-01-31 15:32:05 +0200

    Autopep8 and pylint

--- tdd/test_mycrypt.py
+++ tdd/test_mycrypt.py
@@ -7,9 +7,9 @@ import timeit
 
 ''' Unit tests for mycrypt function. Basically ROT13, but also
 capitalize or uncapitalize, and for numbers, replace with shifted
-versions. 
+versions.
 
-tr 'A-Za-z0-9!"#€%&/()=' 'n-za-mN-ZA-M!"#€%&/()=0-9' 
+tr 'A-Za-z0-9!"#€%&/()=' 'n-za-mN-ZA-M!"#€%&/()=0-9'
 
 If characters outside allowed ones are used as input, raise ValueError'''
 
@@ -17,7 +17,7 @@ If characters outside allowed ones are used as input, raise ValueError'''
 @pytest.mark.parametrize("test_input,expected", [
     ("a", "N"),
     ("b", "O"),
-    ("123",'!"#'),
+    ("123", '!"#'),
     ("4", u'€')
 ])
 def test_encode(test_input, expected):
@@ -25,18 +25,19 @@ def test_encode(test_input, expected):
 
 
 @pytest.mark.parametrize("test_input", [
-    'a', 'b', 'c','abbaacdc',u'!#€%#€%#€%#€%#'
+    'a', 'b', 'c', 'abbaacdc', u'!#€%#€%#€%#€%#'
 ])
 def test_encode_decode(test_input):
     assert(mycrypt.decode(mycrypt.encode(test_input))) == test_input
 
-@pytest.mark.parametrize("invalid_input", ['+','Ö','abcÖÄÖ'])
+
+@pytest.mark.parametrize("invalid_input", ['+', 'Ö', 'abcÖÄÖ'])
 def test_invalid_char(invalid_input):
     with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
 
-@pytest.mark.parametrize("invalid_input", [None, [6,7]])
-def test_invalid_char(invalid_input):
+
+@pytest.mark.parametrize("invalid_input", [None, [6, 7]])
+def test_invalid_types(invalid_input):
     with pytest.raises(TypeError):
         mycrypt.encode(invalid_input)
-        
